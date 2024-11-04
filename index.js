64bf83a09d36376f09ecd45ec583bbc3f04493b5
Domantas <domasp902@gmail.com>
2024-11-04 18:14:41 +0200

    .

--- index.js
+++ index.js
@@ -107,6 +107,6 @@ app.post(
 
 // Start the server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is running on port ${PORT}`);
 });
